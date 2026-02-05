d618f25114d5e87c97478e081f4019ba044534ac
Xing-Heyu <2859602290@qq.com>
2026-02-05 21:44:31 +0800

    Update module description and import core functions

--- idle_sense/__init__.py
+++ idle_sense/__init__.py
@@ -1,4 +1,8 @@
 """
-idle-sense - 跨平台空闲状态检测
+idle-sense: 跨平台空闲检测库
 """
+
+from .core import is_idle, get_system_status, get_platform
+
 __version__ = "0.1.0"
+__all__ = ['is_idle', 'get_system_status', 'get_platform']
