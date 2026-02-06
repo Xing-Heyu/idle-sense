3926276daff70a0c761af3f05343aad09f14e2bc
Xing-Heyu <2859602290@qq.com>
2026-02-07 00:18:32 +0800

    Create __init__.py

---
+++ demo/__init__.py
@@ -0,0 +1,12 @@
+"""
+demo - 演示脚本集合
+展示闲置计算加速器的完整工作流程
+"""
+
+__version__ = "1.0.0"
+__all__ = [
+    'demo_single_machine',
+    'demo_local_network', 
+    'demo_full_workflow',
+    'demo_web_interface'
+]
