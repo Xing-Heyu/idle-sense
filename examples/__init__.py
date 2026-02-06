09d732c11f3236197a87e6e184e312c06e971382
Xing-Heyu <2859602290@qq.com>
2026-02-07 00:02:11 +0800

    Create __init__.py

---
+++ examples/__init__.py
@@ -0,0 +1,13 @@
+"""
+examples - 示例任务集合
+演示各种计算任务的示例
+"""
+
+__version__ = "1.0.0"
+__all__ = [
+    'simple_calculation',
+    'data_processing', 
+    'math_computation',
+    'simulation',
+    'benchmark'
+]
